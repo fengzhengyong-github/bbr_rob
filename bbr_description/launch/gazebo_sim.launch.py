9e2cddc9de248e22a47c39786e3f2aaf7accd39a
Zhengyong Feng <38576259+fengzhengyong-github@users.noreply.github.com>
2025-06-05 23:13:46 +0800

    Add files via upload

--- bbr_description/launch/gazebo_sim.launch.py
+++ bbr_description/launch/gazebo_sim.launch.py
@@ -5,15 +5,17 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     # 获取默认路径
-    robot_name_in_model = "bbr_car"
-    urdf_tutorial_path = get_package_share_directory('bbr_car')
-    default_model_path = urdf_tutorial_path + '/urdf/bbr_car.xacro'
-    default_rviz_config_path = urdf_tutorial_path + '/config/rviz/gazebo_launch.rviz'
-    default_world_path = urdf_tutorial_path + '/world/custom_room.world'
+    robot_name_in_model = "bbr_rob"
+    urdf_tutorial_path = get_package_share_directory('bbr_rob')
+    default_model_path = urdf_tutorial_path + '/bbr_description/urdf/bbr_rob.xacro'
+    default_rviz_config_path = urdf_tutorial_path + '/bbr_description/config/rviz/gazebo_launch.rviz'
+    default_world_path = urdf_tutorial_path + '/bbr_description/world/custom_room.world'
+   
     # 为 Launch 声明参数
     action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
         name='model', default_value=str(default_model_path),
         description='URDF 的绝对路径')
+   
     # 获取文件内容生成新的参数
     robot_description = launch_ros.parameter_descriptions.ParameterValue(
         launch.substitutions.Command(
@@ -39,6 +41,7 @@ def generate_launch_description():
       	# 传递参数
         launch_arguments=[('world', default_world_path),('verbose','true')]
     )
+  
     # 请求 Gazebo 加载机器人
     spawn_entity_node = launch_ros.actions.Node(
         package='gazebo_ros',
--- bbr_description/launch/rviz_display.launch.py
+++ bbr_description/launch/rviz_display.launch.py
@@ -5,24 +5,28 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     # 获取默认路径
-    urdf_tutorial_path = get_package_share_directory('bbr_car')
-    default_model_path = urdf_tutorial_path + '/urdf/bbr_car.xacro'
-    default_rviz_config_path = urdf_tutorial_path + '/config/rviz/display_model.rviz'
+    urdf_tutorial_path = get_package_share_directory('bbr_rob')
+    default_model_path = urdf_tutorial_path + '/bbr_description//urdf/bbr_rob.xacro'
+    default_rviz_config_path = urdf_tutorial_path + '/bbr_description//config/rviz/display_model.rviz'
+   
     # 为 Launch 声明参数
     action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
         name='model', default_value=str(default_model_path),
         description='URDF 的绝对路径')
+   
     # 获取文件内容生成新的参数
     robot_description = launch_ros.parameter_descriptions.ParameterValue(
         launch.substitutions.Command(
             ['xacro ', launch.substitutions.LaunchConfiguration('model')]),
         value_type=str)
+   
     # 状态发布节点
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': robot_description}]
     )
+    
     # 关节状态发布节点
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
@@ -34,6 +38,7 @@ def generate_launch_description():
         executable='rviz2',
         arguments=['-d', default_rviz_config_path]
     )
+    
     return launch.LaunchDescription([
         action_declare_arg_mode_path,
         joint_state_publisher_node,
